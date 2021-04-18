d211cd9beca6d3742a7c847991ae3527319ec327
KiraBeleaua <necula.adrian2009@gmail.com>
2021-04-18 14:02:43 +0300

    rezolvat 4 probleme

---
+++ PBINFO/1260-asii.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, sum;
+    cin >> a >> b;
+    sum = a - b;
+    cout << sum;
+}
+//https://www.pbinfo.ro/probleme/1258/scadere2
---
+++ PBINFO/939-sum00.cpp
@@ -0,0 +1,15 @@
+https://www.pbinfo.ro/detalii-evaluare/16346368
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cin>>a;
+    cin>>b;
+    cout<<a+b;
+        return 0;
+}
